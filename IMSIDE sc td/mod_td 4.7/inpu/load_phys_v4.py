9e033a7f35274d7d1503d613fde62ce448806497
Prayla Barli <praylabarli@gmail.com>
2026-07-08 16:52:57 +0200

    add new physical setting

--- IMSIDE sc td/mod_td 4.7/inpu/load_phys_v4.py
+++ IMSIDE sc td/mod_td 4.7/inpu/load_phys_v4.py
@@ -695,7 +695,115 @@ def phys_LOI2():
         (choice_bottomslip_st,choice_bottomslip_ti,choice_viscosityv_st,choice_viscosityv_ti,choice_diffusivityv_st,choice_diffusivityv_ti,choice_diffusivityh_st)
 
 
-def phys_GUA_notid():
+def phys_GUA_notide_Kh42():
+    # same as phys_GUA_notide(), but with constant horizontal diffusivity Kh_st = 42 instead of 'cub'
+
+    #numerical parameters
+    N   = 5
+    Lsc = 1000
+    nz  = 51 #vertical step
+    nt  = 121 #time step iu tidal cylce - only for plot
+    theta = 0.5
+
+    #physical parameters, subtidal
+    Ut    = 1.15
+
+    Av_st = None #0.0027
+    cv_st = 1e-4
+
+    Kv_st = None
+    Sc_st = 2.2
+
+    sf_st = None
+    rr_st = 1/2
+
+    Kh_st = 42
+    ch_st = None
+
+    #physical parameters, tidal
+    Av_ti = [None]  #0.06
+    cv_ti = [1.2e-3]  #1.2e-3
+
+    Kv_ti = [None]
+    Sc_ti = [1.9]
+
+    sf_ti = [None] #+ np.inf
+    rr_ti = [1/2] #+ np.inf
+
+    Kh_ti = [20]
+
+
+    #choiches regarding parametrizations
+    #bottom slip
+    choice_bottomslip_st = 'rr'
+    choice_bottomslip_ti = 'rr'
+    choice_viscosityv_st = 'cuh'#'constant'
+    choice_viscosityv_ti = 'cuh'#'cuh'
+    choice_diffusivityv_st = 'as'
+    choice_diffusivityv_ti = 'as'#'as'
+    choice_diffusivityh_st = 'constant'#'cub'
+    #choice_diffusivityh_ti = 'constant' #this has to be constant I guess.
+
+
+    return (N, Lsc, nz, nt, theta) , (Ut, Av_st, cv_st, Kv_st, Sc_st, sf_st, rr_st, Kh_st, ch_st) , (Av_ti, cv_ti, Kv_ti, Sc_ti, sf_ti, rr_ti, Kh_ti), \
+        (choice_bottomslip_st,choice_bottomslip_ti,choice_viscosityv_st,choice_viscosityv_ti,choice_diffusivityv_st,choice_diffusivityv_ti,choice_diffusivityh_st)
+
+
+def phys_GUA_notide_Kh22():
+    # same as phys_GUA_notide(), but with constant horizontal diffusivity Kh_st = 22 instead of 'cub'
+
+    #numerical parameters
+    N   = 5
+    Lsc = 1000
+    nz  = 51 #vertical step
+    nt  = 121 #time step iu tidal cylce - only for plot
+    theta = 0.5
+
+    #physical parameters, subtidal
+    Ut    = 1.15
+
+    Av_st = None #0.0027
+    cv_st = 1e-4
+
+    Kv_st = None
+    Sc_st = 2.2
+
+    sf_st = None
+    rr_st = 1/2
+
+    Kh_st = 22
+    ch_st = None
+
+    #physical parameters, tidal
+    Av_ti = [None]  #0.06
+    cv_ti = [1.2e-3]  #1.2e-3
+
+    Kv_ti = [None]
+    Sc_ti = [1.9]
+
+    sf_ti = [None] #+ np.inf
+    rr_ti = [1/2] #+ np.inf
+
+    Kh_ti = [20]
+
+
+    #choiches regarding parametrizations
+    #bottom slip
+    choice_bottomslip_st = 'rr'
+    choice_bottomslip_ti = 'rr'
+    choice_viscosityv_st = 'cuh'#'constant'
+    choice_viscosityv_ti = 'cuh'#'cuh'
+    choice_diffusivityv_st = 'as'
+    choice_diffusivityv_ti = 'as'#'as'
+    choice_diffusivityh_st = 'constant'#'cub'
+    #choice_diffusivityh_ti = 'constant' #this has to be constant I guess.
+
+
+    return (N, Lsc, nz, nt, theta) , (Ut, Av_st, cv_st, Kv_st, Sc_st, sf_st, rr_st, Kh_st, ch_st) , (Av_ti, cv_ti, Kv_ti, Sc_ti, sf_ti, rr_ti, Kh_ti), \
+        (choice_bottomslip_st,choice_bottomslip_ti,choice_viscosityv_st,choice_viscosityv_ti,choice_diffusivityv_st,choice_diffusivityv_ti,choice_diffusivityh_st)
+
+
+def phys_GUA_notide():
     #lots of options here possible. 
     
     #numerical parameters
@@ -706,10 +814,11 @@ def phys_GUA_notid():
     theta = 0.5
 
     #physical parameters, subtidal
-    Ut    = 1
+    Ut    = 1.15
     
     Av_st = None #0.0027
     cv_st = 1e-4
+    # cv_st = 20.7e-5
     
     Kv_st = None 
     Sc_st = 2.2
@@ -717,7 +826,64 @@ def phys_GUA_notid():
     sf_st = None
     rr_st = 1/2
     
-    Kh_st = 22
+    Kh_st = None
+    # ch_st = 77.3e-3
+    ch_st = 150e-3
+
+    #physical parameters, tidal
+    Av_ti = [None]  #0.06
+    cv_ti = [1.2e-3]  #1.2e-3
+    
+    Kv_ti = [None]
+    Sc_ti = [1.9]
+    
+    sf_ti = [None] #+ np.inf
+    rr_ti = [1/2] #+ np.inf
+    
+    Kh_ti = [20]
+
+    
+    #choiches regarding parametrizations
+    #bottom slip
+    choice_bottomslip_st = 'rr'
+    choice_bottomslip_ti = 'rr'
+    choice_viscosityv_st = 'cuh'#'constant'
+    choice_viscosityv_ti = 'cuh'#'cuh'
+    choice_diffusivityv_st = 'as'
+    choice_diffusivityv_ti = 'as'#'as'
+    choice_diffusivityh_st = 'cub'#'cub'
+    #choice_diffusivityh_ti = 'constant' #this has to be constant I guess. 
+    
+    
+    return (N, Lsc, nz, nt, theta) , (Ut, Av_st, cv_st, Kv_st, Sc_st, sf_st, rr_st, Kh_st, ch_st) , (Av_ti, cv_ti, Kv_ti, Sc_ti, sf_ti, rr_ti, Kh_ti), \
+        (choice_bottomslip_st,choice_bottomslip_ti,choice_viscosityv_st,choice_viscosityv_ti,choice_diffusivityv_st,choice_diffusivityv_ti,choice_diffusivityh_st)
+
+
+def phys_GUA_tune(Kh_st):
+    #lots of options here possible. 
+    
+    #numerical parameters
+    N   = 5
+    Lsc = 1000
+    nz  = 51 #vertical step 
+    nt  = 121 #time step iu tidal cylce - only for plot
+    theta = 0.5
+
+    #physical parameters, subtidal
+    Ut    = 1.15
+    
+    Av_st = None #0.0027
+    cv_st = 1e-4
+    # cv_st = 20.7e-5
+    
+    Kv_st = None 
+    Sc_st = 2.2
+    
+    sf_st = None
+    rr_st = 1/2
+    
+    Kh_st = Kh_st
+    # ch_st = 77.3e-3
     ch_st = None
 
     #physical parameters, tidal
